67d967a85dd46c4471b46d037fbd8127df48077d
justanotheruserhi <yuoturellepikie@gmail.com>
2023-11-16 19:07:28 +0600

    Header added, doctor showcase WIP

---
+++ iOS-Test-Project/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  iOS-test-project
+//
+//  Created by Effective on 16.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Hello,")
+                      .font(Font.custom("Poppins", size: 16))
+                      .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
+                    Text("Hi James").font(
+                        Font.custom("Poppins", size: 20)
+                        .weight(.bold)
+                        )
+                        .foregroundColor(Color(red: 0.05, green: 0.11, blue: 0.2))
+                }
+                Spacer()
+                ProfileImage()
+            }.padding(.leading, 24)
+             .padding(.trailing, 24)
+            DoctorNotif()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Test-Project/ProfileImage.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileImage.swift
+//  iOS-test-project
+//
+//  Created by Effective on 16.11.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProfileImage : View {
+    var body : some View {
+        Image("Frame").clipShape(Circle())
+    }
+}
